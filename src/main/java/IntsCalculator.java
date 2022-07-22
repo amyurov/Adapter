eacc8d2b682e7292aa6e493833ce4e0aac152c89
Артем Юров <am.yurov@mail.ru>
2022-07-22 14:09:48 +0300

    First commit

---
+++ src/main/java/IntsCalculator.java
@@ -0,0 +1,35 @@
+public class IntsCalculator implements Ints{
+
+    private final Calculator target;
+
+    public IntsCalculator() {
+        target = new Calculator();
+    }
+
+    @Override
+    public int sum(int arg0, int arg1) {
+       return (int) Math.round(target.newFormula()
+               .addOperand(arg0)
+               .addOperand(arg1)
+               .calculate(Calculator.Operation.SUM)
+               .result());
+    }
+
+    @Override
+    public int mult(int arg0, int arg1) {
+        return (int) Math.round(target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.MULT)
+                .result());
+    }
+
+    @Override
+    public int pow(int a, int b) {
+        return (int) Math.round(target.newFormula()
+                .addOperand(a)
+                .addOperand(b)
+                .calculate(Calculator.Operation.POW)
+                .result());
+    }
+}
